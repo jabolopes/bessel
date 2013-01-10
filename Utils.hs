f408c1965addea64d3fbfb15c4072e3a92efb3eb
José Lopes <jabolopes@gmail.com>
2013-01-10 01:22:29 +0100

    Removed useless function from 'Utils' module.

--- Utils.hs
+++ Utils.hs
@@ -1,20 +1,10 @@
 module Utils where
 
-import Data.List
-import qualified Data.Sequence as Sequence
-
 
 cross :: [[a]] -> [[a]]
 cross [] = []
 cross [lst] = [[x] | x <- lst]
 cross (lst:lsts) = [x:l | x <- lst, l <- cross lsts]
-
-
-groupPair :: (a -> a -> Bool) -> [(a,b)] -> [(a,[b])]
-groupPair _  [] =  []
-groupPair eq (x@(x1,x2):xs) =
-    (x1, x2:map snd ys):groupPair eq zs
-    where (ys,zs) = span (\(x1',_) -> eq x1 x1') xs
 
 
 primeId :: String -> String
