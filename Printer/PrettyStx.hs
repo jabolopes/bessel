68dc50f0ecece29e0bd54755dfd8f23f1cd1ab5b
José Lopes <jabolopes@gmail.com>
2013-03-17 00:17:38 +0100

    Improved pretty printer.

--- Printer/PrettyStx.hs
+++ Printer/PrettyStx.hs
@@ -112,13 +112,13 @@ printStxM (TypeStx name stxs) =
                  printStxs stxs
 
 printStxM (TypeMkStx name) =
-    putPrinter $ "mk:" ++ show name
+    putPrinter $ "mk' " ++ name
 
 printStxM (TypeUnStx) =
-    putPrinter $ "un:"
+    putPrinter $ "un"
 
 printStxM (TypeIsStx name) =
-    putPrinter $ "is:" ++ show name
+    putPrinter $ "is' " ++ name
 
 printStxM (WhereStx stx stxs) =
     do printStxM stx
