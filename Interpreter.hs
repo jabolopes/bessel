351a2af2e5b55bc98506376d703233a01ed96da0
Jose A. Lopes <jabolopes@gmail.com>
2013-10-03 23:18:05 +0200

    Improve code

--- Interpreter.hs
+++ Interpreter.hs
@@ -1,41 +1,35 @@
-{-# LANGUAGE NamedFieldPuns, ParallelListComp #-}
+{-# LANGUAGE ParallelListComp #-}
 module Interpreter where
 
 import Control.Monad.State
 import Data.Functor ((<$>))
-import Data.Map as Map (Map)
-import qualified Data.Map as Map ((!), fromList, insert, keys, lookup, toList, union)
-import Data.Maybe (catMaybes, isNothing, mapMaybe)
+import qualified Data.Map as Map (fromList)
+import Data.Maybe (isNothing, mapMaybe)
 import Data.Either (lefts, rights)
 
 import Data.Definition (Definition(..))
 import qualified Data.Definition as Definition
-import qualified Data.Env as Env (initial, empty, getBinds)
+import qualified Data.Env as Env (initial)
 import Data.FileSystem (FileSystem)
 import qualified Data.FileSystem as FileSystem
 import Data.SrcFile
-import qualified Data.SrcFile as SrcFile (updateDefinitions, defsAsc, deps, name)
+import qualified Data.SrcFile as SrcFile (updateDefinitions, defsAsc)
 import Data.Expr
-import Data.QualName (QualName)
 import qualified Data.QualName as QualName (fromQualName)
 import Data.Symbol
 import Monad.InterpreterM
-import Utils
-
 
 evalM :: Expr -> InterpreterM Val
 evalM (CharE c) = return $ CharVal c
 evalM (IntE i) = return $ IntVal i
 evalM (RealE d) = return $ RealVal d
 evalM (SeqE exprs) = SeqVal <$> mapM evalM exprs
-
 evalM (IdE str) =
     do msym <- findBindM (QualName.fromQualName str)
        case msym of
          Just val -> return val
          Nothing -> error $ "Interpreter.evalM(IdE): unbound symbols must be caught by the renamer" ++
                             "\n\n\t str = " ++ QualName.fromQualName str ++ "\n"
-
 evalM (AppE expr1 expr2) =
     do val1 <- evalM expr1
        val2 <- evalM expr2
@@ -46,7 +40,6 @@ evalM (AppE expr1 expr2) =
                       "\n\n\t -> val1 = " ++ show val1 ++
                       "\n\n\t expr2 = " ++ show expr2 ++
                       "\n\n\t -> val2 = " ++ show val2 ++ "\n"
-
 evalM (CondE ms blame) = evalMatches ms
     where evalMatches [] =
               error $ "Interpreter.evalM(CondE): non-exhaustive patterns in " ++ blame
@@ -56,30 +49,24 @@ evalM (CondE ms blame) = evalMatches ms
                  case pred' of
                    BoolVal False -> evalMatches ms
                    _ -> evalM val
-
 evalM (FnDecl Def str body) =
   error "Interpreter.evalM(FnDecl Def ...): recursive functions must be eliminated in previous stages"
-
 evalM (FnDecl NrDef str body) =
     do val <- evalM body
        addBindM str val
        return val
-
 evalM (LambdaE str body) =
     FnVal . closure <$> get
     where closure env val =
               withLexicalEnvM env $ do
                 addBindM str val
                 withEnvM (evalM body)
-
 evalM (MergeE vals) =
   TypeVal <$> SeqVal <$> mapM (evalM . snd) vals
-
 evalM (WhereE expr exprs) =
     withEnvM $ do
       mapM_ evalM exprs
       withEnvM (evalM expr)
-
 
 interpretDefinition :: FileSystem -> Definition -> Definition
 interpretDefinition fs def@Definition { defRen = Right expr } =
