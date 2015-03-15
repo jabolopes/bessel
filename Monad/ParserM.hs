1f929d6ec6eacfd9b05a137e0c0a3ec3ca95c53b
Jose Lopes <jabolopes@google.com>
2015-03-15 16:06:12 +0100

    Improve error message in 'ensureExpr'.

--- Monad/ParserM.hs
+++ Monad/ParserM.hs
@@ -4,8 +4,10 @@ import Control.Monad.Except (throwError)
 import Control.Monad.State
 import Data.Functor ((<$>))
 import Data.LexState
+import qualified Data.PrettyString as PrettyString
 import Data.Source (Source)
 import qualified Data.Source as Source
+import qualified Pretty.Data.Source as Pretty
 
 data ParserState
     = ParserState { psLexerState :: LexState }
@@ -24,5 +26,5 @@ failM err =
 ensureExpr :: Source -> ParserM Source
 ensureExpr val =
   case Source.toSource val of
-    Nothing -> failM "expecting expression instead of pattern"
+    Nothing -> failM $ "expecting expression instead of pattern: " ++ PrettyString.toString (Pretty.docSource val)
     Just expr -> return expr
