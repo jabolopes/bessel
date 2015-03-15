36d4d4c2628c143746dbee62c9757d54080ed1d9
Jose Lopes <jabolopes@google.com>
2015-03-15 19:07:05 +0100

    Fix 'Renamer' test.

--- Test/Stage/Renamer.hs
+++ Test/Stage/Renamer.hs
@@ -3,25 +3,40 @@
 module Test.Stage.Renamer where
 
 import Control.Applicative
-import Control.Monad.State
+import Control.Monad.State hiding (state)
 
-import Data.Expr
+import Data.Expr (DefnKw(..), Expr(..))
+import qualified Data.Expr as Expr
 import qualified Data.PrettyString as PrettyString
 import Data.QualName (QualName (..))
 import Data.Source
 import qualified Parser
 import qualified Pretty.Data.Expr as Pretty
 import qualified Stage.Expander as Expander
+import Stage.Renamer (RenamerState)
 import qualified Stage.Renamer as Renamer
 
 deriving instance Eq DefnKw
 deriving instance Eq Expr
 deriving instance Eq QualName
 
-renameTestFile :: String -> IO Expr
+initialRenamerState :: Monad m => m RenamerState
+initialRenamerState =
+  case snd <$> runStateT getRenamerState Renamer.initialRenamerState of
+    Left err -> fail $ show err
+    Right state -> return state
+  where
+    getRenamerState =
+      do Renamer.addFnSymbolM "true#" "true#"
+         Renamer.addFnSymbolM "false#" "false#"
+         Renamer.addFnSymbolM "+" "+"
+         Renamer.addFnSymbolM "isInt" "isInt#"
+
+renameTestFile :: String -> IO [Expr]
 renameTestFile filename =
   do expr <- expandFile
-     case fst <$> runState (Renamer.renameM expr) Renamer.initialRenamerState of
+     state <- initialRenamerState
+     case fst <$> runStateT (Renamer.renameM expr) state of
        Left err -> fail $ show err
        Right expr' -> return expr'
   where
@@ -38,52 +53,59 @@ renameTestFile filename =
            Right exprs -> return (head exprs)
 
 -- edit: Use pretty strings instead
-expect :: Expr -> String -> IO ()
+expect :: [Expr] -> String -> IO ()
 expect expected filename =
   expect' =<< renameTestFile filename
   where
     expect' actual
       | expected == actual = return ()
       | otherwise =
-        fail $ "In: " ++ filename ++ "\n" ++
-               "Expected: " ++ "\n" ++ PrettyString.toString (Pretty.docExpr Pretty.ExpDocT expected) ++ "\n" ++
-               "Actual: " ++ "\n" ++ PrettyString.toString (Pretty.docExpr Pretty.ExpDocT actual)
+        fail $ "Renamer" ++ "\n" ++
+               "In: " ++ filename ++ "\n" ++
+               "Expected: " ++ "\n" ++ PrettyString.toString (Pretty.docExprList expected) ++ "\n" ++
+               "Actual: " ++ "\n" ++ PrettyString.toString (Pretty.docExprList actual)
 
 testRenamer :: IO ()
 testRenamer =
-  do expect expected1 "Test/TestData1.bsl"
-     expect expected2 "Test/TestData2.bsl"
+  do expect [expected1] "Test/TestData1.bsl"
+     expect [expected2] "Test/TestData2.bsl"
   where
     expected1 =
       FnDecl NrDef "f10"
       (LambdaE "x#01"
-       (CondE [(AppE (IdE (QualName {fromQualName = "isInt"})) (IdE (QualName {fromQualName = "x#01"})),
-                LambdaE "y#12"
-                (CondE [(AppE (IdE (QualName {fromQualName = "isInt"})) (IdE (QualName {fromQualName = "y#12"})),
-                         LetE (FnDecl NrDef "x3" (IdE (QualName {fromQualName = "x#01"})))
-                         (LetE (FnDecl NrDef "y4" (IdE (QualName {fromQualName = "y#12"})))
-                          (LetE
-                           (FnDecl NrDef "f25"
-                            (LambdaE "z#26"
-                             (CondE [(AppE (LambdaE "_7" (IdE (QualName {fromQualName = "true#"}))) (IdE (QualName {fromQualName = "z#26"})),
-                                      LetE (FnDecl NrDef "z8" (IdE (QualName {fromQualName = "z#26"})))
-                                      (AppE (AppE (IdE (QualName {fromQualName = "+"})) (IdE (QualName {fromQualName = "z8"}))) (IdE (QualName {fromQualName = "y4"}))))]
-                              "f2")))
-                           (AppE (IdE (QualName {fromQualName = "f25"})) (IdE (QualName {fromQualName = "x3"}))))))]
-                 "f1"))]
-        "f1"))
+       (LambdaE "y#12"
+        (CondE
+         [(CondE
+           [(AppE (Expr.idE "isInt#") (Expr.idE "x#01"),
+             CondE [(AppE (Expr.idE "isInt#") (Expr.idE "y#12"),Expr.idE "true#"),
+                    (Expr.idE "true#",Expr.idE "false#")]
+             "irrefutable 'and' pattern"),
+            (Expr.idE "true#",Expr.idE "false#")]
+           "irrefutable 'and' pattern",
+           LetE (FnDecl NrDef "x3" (Expr.idE "x#01"))
+           (LetE (FnDecl NrDef "y4" (Expr.idE "y#12"))
+            (LetE
+             (FnDecl NrDef "f25"
+              (LambdaE "z#26"
+               (CondE
+                [(AppE (LambdaE "_7" (Expr.idE "true#")) (Expr.idE "z#26"),
+                  LetE (FnDecl NrDef "z8" (Expr.idE "z#26"))
+                  (AppE (AppE (Expr.idE "+") (Expr.idE "z8")) (Expr.idE "y4")))]
+                "f2")))
+             (AppE (Expr.idE "f25") (Expr.idE "x3")))))]
+         "f1")))
 
     expected2 =
       FnDecl NrDef "f10"
       (LambdaE "x#01"
-       (CondE [(AppE (IdE (QualName {fromQualName = "isInt"})) (IdE (QualName {fromQualName = "x#01"})),
-                LetE (FnDecl NrDef "x2" (IdE (QualName {fromQualName = "x#01"})))
+       (CondE [(AppE (Expr.idE "isInt#") (Expr.idE "x#01"),
+                LetE (FnDecl NrDef "x2" (Expr.idE "x#01"))
                 (LetE (FnDecl NrDef "f23"
                        (LambdaE "z#14"
-                        (CondE [(AppE (LambdaE "_5" (IdE (QualName {fromQualName = "true#"}))) (IdE (QualName {fromQualName = "z#14"})),
-                                 LetE (FnDecl NrDef "z6" (IdE (QualName {fromQualName = "z#14"})))
-                                 (AppE (AppE (IdE (QualName {fromQualName = "+"})) (IdE (QualName {fromQualName = "z6"}))) (IntE 1)))]
+                        (CondE [(AppE (LambdaE "_5" (Expr.idE "true#")) (Expr.idE "z#14"),
+                                 LetE (FnDecl NrDef "z6" (Expr.idE "z#14"))
+                                 (AppE (AppE (Expr.idE "+") (Expr.idE "z6")) (IntE 1)))]
                          "f2")))
                  (LetE (FnDecl NrDef "y7" (IntE 0))
-                  (AppE (IdE (QualName {fromQualName = "f23"})) (IdE (QualName {fromQualName = "y7"}))))))]
+                  (AppE (Expr.idE "f23") (Expr.idE "y7")))))]
         "f1"))
