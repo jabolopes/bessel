82d6394490bf453c87b9ed5da2da97a3c1416a5e
Jose Lopes <jabolopes@google.com>
2015-04-06 01:26:50 +0200

    Improve style.

--- Data/GraphUtils.hs
+++ Data/GraphUtils.hs
@@ -5,20 +5,18 @@ import Data.Tree (levels)
 
 import Utils
 
-
+hasCycle :: [[[a]]] -> Maybe [a]
 hasCycle [] = Nothing
 hasCycle (is:iss)
     | singleton is = hasCycle iss
     | otherwise = Just (concat is)
 
-
 replaceIndexes :: [a] -> [Int] -> [a]
 replaceIndexes nodes is = [ nodes !! (i - 1) | i <- is ]
 
-
 acyclicTopSort :: [a] -> [Edge] -> Either [a] [a]
 acyclicTopSort nodes edges =
-    let graph = buildG (1, length nodes) edges in
-    case hasCycle $ map levels $ scc graph of
-      Nothing -> Right $ reverse $ replaceIndexes nodes $ topSort graph
-      Just is -> Left (replaceIndexes nodes is)
+  let graph = buildG (1, length nodes) edges in
+  case hasCycle $ map levels $ scc graph of
+    Nothing -> Right $ reverse $ replaceIndexes nodes $ topSort graph
+    Just is -> Left (replaceIndexes nodes is)
