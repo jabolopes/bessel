ab98c76a183918fd94cfecbe3eb28d7faff2f90d
Jose Lopes <jabolopes@gmail.com>
2019-02-01 00:12:58 +0100

    Fix many issues with pretty printing patterns.

--- Pretty/Data/Source.hs
+++ Pretty/Data/Source.hs
@@ -4,9 +4,8 @@ import Prelude hiding ((<>))
 
 import Data.PrettyString (PrettyString, (<>), (<+>), ($+$))
 import qualified Data.PrettyString as PrettyString
-import Data.Name (Name)
 import qualified Data.Name as Name
-import Data.Source
+import Data.Source (Source(..))
 
 isParens :: Bool -> Source -> Bool
 isParens right AppS {}    = right
@@ -18,25 +17,70 @@ isParens _     PatS {}    = False
 isParens _     RealS {}   = False
 isParens _     SeqS {}    = False
 isParens _     StringS {} = False
+isParens _     TupleS {}  = False
 isParens _ _              = True
 
-docParens :: Bool -> Source -> PrettyString
-docParens right src
-  | isParens right src = PrettyString.parens $ docSource src
-  | otherwise = docSource src
+docParens :: Bool -> (Source -> PrettyString) -> Source -> PrettyString
+docParens right fn src
+  | isParens right src = PrettyString.parens $ fn src
+  | otherwise = fn src
 
-docPat :: Name -> Maybe Source -> PrettyString
-docPat = docPattern
-  where
-    docPattern binder Nothing
-      | Name.isEmptyName binder = PrettyString.text "@"
-      | otherwise = PrettyString.text (show binder)
-    docPattern binder (Just src) =
-      PrettyString.text (show binder) <> PrettyString.text "@" <> docSource src
+docSimplePattern :: Source -> PrettyString
+docSimplePattern (AndS src1 src2) =
+  docSimplePattern $ BinOpS "&&" src1 src2
+docSimplePattern (AppS src1 src2) =
+  docPattern src1 <+> docPattern src2
+docSimplePattern (BinOpS op src1 src2) =
+  docPattern src1 <+>
+  PrettyString.text op <+>
+  docPattern src2
+docSimplePattern src@CharS {} =
+  docSource src
+docSimplePattern src@IdS {} =
+  docSource src
+docSimplePattern src@IntS {} =
+  docSource src
+docSimplePattern (OrS src1 src2) =
+  docSimplePattern $ BinOpS "||" src1 src2
+docSimplePattern src@RealS {} =
+  docSource src
+docSimplePattern src@StringS {} =
+  docSource src
+docSimplePattern (SeqS srcs) =
+  PrettyString.text "[" <>
+  PrettyString.sep (PrettyString.intercalate (PrettyString.text ",") (map docPattern srcs)) <>
+  PrettyString.text "]"
+docSimplePattern (TupleS []) =
+  PrettyString.text "()"
+docSimplePattern (TupleS [src]) =
+  docPattern src
+docSimplePattern (TupleS srcs) =
+  PrettyString.text "(" <>
+  PrettyString.sep (PrettyString.intercalate (PrettyString.text ",") (map docPattern srcs)) <>
+  PrettyString.text ")"
+docSimplePattern src =
+  error . PrettyString.toString $
+    PrettyString.text "docSimplePattern: incomplete pattern matching"
+    $+$
+    docSource src
+
+docPattern :: Source -> PrettyString
+docPattern (PatS binder (Just src@IdS {})) =
+  PrettyString.text (show binder) <> PrettyString.text "@" <> docSource src
+docPattern (PatS binder (Just guard)) =
+  PrettyString.text (show binder) <> PrettyString.text "@" <> docParens True docSimplePattern guard
+docPattern (PatS binder Nothing)
+  | Name.isEmptyName binder = PrettyString.text "@"
+  | otherwise = PrettyString.text (show binder)
+docPattern src@(IdS name)
+  | not $ Name.isTypeName name =
+    PrettyString.text "@" <> docSimplePattern src
+docPattern src =
+  docParens True docSimplePattern src
 
 docMatch :: ([Source], Source) -> PrettyString
 docMatch (args, body) =
-  PrettyString.sep [PrettyString.sep (map (docParens True) args),
+  PrettyString.sep [PrettyString.sep (map docPattern args),
                     PrettyString.equals,
                     PrettyString.nest . docSource $ body]
 
@@ -47,8 +91,8 @@ docSource :: Source -> PrettyString
 docSource (AndS src1 src2) =
   docSource src1 <+> PrettyString.text "&&" <+> docSource src2
 docSource (AppS src1 src2) =
-  PrettyString.sep [docParens False src1,
-                    PrettyString.nest (docParens True src2)]
+  PrettyString.sep [docParens False docSource src1,
+                    PrettyString.nest (docParens True docSource src2)]
 docSource (BinOpS op src1 src2) =
   docSource src1 <+> PrettyString.text op <+> docSource src2
 docSource (CharS c) =
@@ -109,8 +153,8 @@ docSource (ModuleS name uses defns) =
       docUse (use, Name.empty) <+> PrettyString.text "as" <+> PrettyString.text (show qual)
 docSource (OrS src1 src2) =
   docSource src1 <+> PrettyString.text "||" <+> docSource src2
-docSource (PatS binder guard) =
-  docPat binder guard
+docSource src@PatS {} =
+  docPattern src
 docSource (RealS r) =
   PrettyString.double r
 docSource (SeqS ms) =
@@ -130,11 +174,11 @@ docSource (TypeDeclS typeName tags) =
     $+$
     PrettyString.nest docTags
   where
-    -- TODO: Distinction between first constructor and rest doesn't look good.
-    docTag True (consName, consPat) =
-      PrettyString.text (show consName) <+> docSource consPat
-    docTag False (consName, consPat) =
-      PrettyString.text "|" <+> PrettyString.text (show consName) <+> docSource consPat
+    -- TODO: Distinction between first tag and rest doesn't look good.
+    docTag True (tagName, pat) =
+      PrettyString.text (show tagName) <+> docPattern pat
+    docTag False (tagName, pat) =
+      PrettyString.text "|" <+> PrettyString.text (show tagName) <+> docPattern pat
 
     docTags =
       PrettyString.vcat $ (docTag True (head tags)):map (docTag False) (tail tags)
